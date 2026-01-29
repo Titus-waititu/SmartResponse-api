ea4ca0a8193a2aefbaec6c855b775eb4a6ad96d1
Titus-waititu <tituswaititu96@gmail.com>
2026-01-29 18:18:02 +0300

    refactor(auth): simplify type assertions in decorators and guards

--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -2,8 +2,8 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
   (data: string | undefined, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest() as { user: any };
-    if (!data) return request.user as any;
-    return request.user?.[data] as any;
+    const request = ctx.switchToHttp().getRequest();
+    if (!data) return request.user;
+    return request.user?.[data];
   },
 );
